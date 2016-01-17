0f1f76e6399caa1769a69ed38cd483910d106d18
Abigail Watson <awatson1978@gmail.com>
2016-01-16 18:03:51 -0800

    reconciled merge conflicts

--- components/eulaPage/eulaPage.js
+++ components/eulaPage/eulaPage.js
@@ -1,17 +1,9 @@
 Router.route('/eula', {
   template: 'eulaPage',
-<<<<<<< HEAD
   name: 'eulaRoute'
-=======
-  name: 'eulaPage'
->>>>>>> 4cd7d2661bc62df1cc045559285026534605d74b
 });
 
 Router.route('/terms-of-use', {
   template: 'eulaPage',
-<<<<<<< HEAD
   name: 'termsOfUseRoute'
-=======
-  name: 'eulaPage'
->>>>>>> 4cd7d2661bc62df1cc045559285026534605d74b
 });
