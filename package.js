702b3b7951111f91c4cf4b3155afaff0321cac87
Abigail Watson <awatson1978@gmail.com>
2016-02-14 12:24:00 -0800

    bump glass-ui dep

--- package.js
+++ package.js
@@ -12,7 +12,7 @@ Package.onUse(function(api) {
     'meteor-platform',
     'templating',
     'clinical:router@2.0.17',
-    'clinical:glass-ui@1.3.5',
+    'clinical:glass-ui@1.3.9',
     'clinical:theming@0.4.3',
     'grove:less@0.1.1',
     'session',
