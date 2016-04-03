31f41fe30d75f91771cba55d4a427265d1d212e4
Abigail Watson <awatson1978@gmail.com>
2016-04-03 13:04:11 -0700

    circle.yml; StaticPagesTests

---
+++ tests/gagarin/StaticPagesTests.js
@@ -0,0 +1,16 @@
+describe('clinical:error-pages', function () {
+  var server = meteor();
+  var client = browser(server);
+
+  it('Static pages exist on client.', function () {
+    return client.execute(function () {
+
+      expect(Template.aboutPage).to.exist();
+      expect(Template.eulaPage).to.exist();
+      expect(Template.faqPage).to.exist();
+      expect(Template.marketingPage).to.exist();
+      expect(Template.privacyPage).to.exist();
+      expect(Template.supportPage).to.exist();
+    });
+  });
+});
